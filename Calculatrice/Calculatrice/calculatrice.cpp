8054ad02ca58fd860b0b2f0e5305f602b3e23fcb
aallard12 <allantoine.72@gmail.com>
2025-11-07 10:50:44 +0100

    CalculatriceV1

--- Calculatrice/Calculatrice/calculatrice.cpp
+++ Calculatrice/Calculatrice/calculatrice.cpp
@@ -1,5 +1,6 @@
 #include "calculatrice.h"
 #include "ui_calculatrice.h"
+#include <QJSEngine>
 
 Calculatrice::Calculatrice(QWidget *parent)
     : QWidget(parent)
@@ -11,5 +12,115 @@ Calculatrice::Calculatrice(QWidget *parent)
 Calculatrice::~Calculatrice()
 {
     delete ui;
+}
+
+
+void Calculatrice::on_pushButton_1_clicked()
+{
+    QString expression = ui->lineEditAfficheur->text();
+    ui->lineEditAfficheur->setText(expression + "1");
+}
+
+
+void Calculatrice::on_pushButton_2_clicked()
+{
+    QString expression = ui->lineEditAfficheur->text();
+    ui->lineEditAfficheur->setText(expression + "2");
+}
+
+
+void Calculatrice::on_pushButton_3_clicked()
+{
+    QString expression = ui->lineEditAfficheur->text();
+    ui->lineEditAfficheur->setText(expression + "3");
+}
+
+
+void Calculatrice::on_pushButton_4_clicked()
+{
+    QString expression = ui->lineEditAfficheur->text();
+    ui->lineEditAfficheur->setText(expression + "4");
+}
+
+
+void Calculatrice::on_pushButton_5_clicked()
+{
+    QString expression = ui->lineEditAfficheur->text();
+    ui->lineEditAfficheur->setText(expression + "5");
+}
+
+
+void Calculatrice::on_pushButton_6_clicked()
+{
+    QString expression = ui->lineEditAfficheur->text();
+    ui->lineEditAfficheur->setText(expression + "6");
+}
+
+
+void Calculatrice::on_pushButton_7_clicked()
+{
+    QString expression = ui->lineEditAfficheur->text();
+    ui->lineEditAfficheur->setText(expression + "7");
+}
+
+
+void Calculatrice::on_pushButton_8_clicked()
+{
+    QString expression = ui->lineEditAfficheur->text();
+    ui->lineEditAfficheur->setText(expression + "8");
+}
+
+
+void Calculatrice::on_pushButton_9_clicked()
+{
+    QString expression = ui->lineEditAfficheur->text();
+    ui->lineEditAfficheur->setText(expression + "9");
+}
+
+
+void Calculatrice::on_pushButton_0_clicked()
+{
+    QString expression = ui->lineEditAfficheur->text();
+    ui->lineEditAfficheur->setText(expression + "0");
+}
+
+
+void Calculatrice::on_pushButton_Egal_clicked()
+{
+    QJSEngine expression;
+    ui->lineEditAfficheur->setText(expression.evaluate(ui->lineEditAfficheur->text()).toString());
+}
+
+void Calculatrice::on_pushButton_Additionner_clicked()
+{
+    QString expression = ui->lineEditAfficheur->text();
+    ui->lineEditAfficheur->setText(expression + "+");
+}
+
+
+void Calculatrice::on_pushButton_Soustraire_clicked()
+{
+    QString expression = ui->lineEditAfficheur->text();
+    ui->lineEditAfficheur->setText(expression + "-");
+}
+
+
+void Calculatrice::on_pushButton_Multiplier_clicked()
+{
+    QString expression = ui->lineEditAfficheur->text();
+    ui->lineEditAfficheur->setText(expression + "*");
+}
+
+
+void Calculatrice::on_pushButton_Diviser_clicked()
+{
+    QString expression = ui->lineEditAfficheur->text();
+    ui->lineEditAfficheur->setText(expression + "/");
+}
+
+
+void Calculatrice::on_pushButton_Effacer_clicked()
+{
+    ui->lineEditAfficheur->clear();
 }
 
--- Calculatrice/Calculatrice/calculatrice.h
+++ Calculatrice/Calculatrice/calculatrice.h
@@ -15,6 +15,39 @@ public:
     Calculatrice(QWidget *parent = nullptr);
     ~Calculatrice();
 
+private slots:
+    void on_pushButton_1_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_4_clicked();
+
+    void on_pushButton_5_clicked();
+
+    void on_pushButton_6_clicked();
+
+    void on_pushButton_Egal_clicked();
+
+    void on_pushButton_7_clicked();
+
+    void on_pushButton_8_clicked();
+
+    void on_pushButton_9_clicked();
+
+    void on_pushButton_0_clicked();
+
+    void on_pushButton_Additionner_clicked();
+
+    void on_pushButton_Soustraire_clicked();
+
+    void on_pushButton_Multiplier_clicked();
+
+    void on_pushButton_Diviser_clicked();
+
+    void on_pushButton_Effacer_clicked();
+
 private:
     Ui::Calculatrice *ui;
 };
